133d5a2e4235907aae1e53ecc7125b1d9b517af9
hamedhi66 <hamed.hakimelahi@gmail.com>
2020-01-11 11:22:30 -0800

    brach update

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -1,8 +1,22 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
+
+
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([34.0522, -118.2437], 4);
+
+//  Add a marker to the map for Los Angeles, California.
+L.circle([34.0522, -118.2437], {
+    radius: 90000
+ }).addTo(map);
+
+ var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(map);
+
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
